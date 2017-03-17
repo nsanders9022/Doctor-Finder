22d18a4a9096f255172fc39f7f48750ca3ec7a57
Nicole Sanders <nsanders9022@gmail.com>
2017-03-17 14:48:07 -0700

    add the doctor's picture to the bio section

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,31 +3,32 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 var displayDoctor = function(doctorData) {
   $(".doctor-info").hide();
   $(".results ul").append('<li id=' + doctorData.profile.last_name +'> <a href="#">' + doctorData.profile.first_name + " " + doctorData.profile.last_name + '</a></li>');
+
   $("li#" + doctorData.profile.last_name).click(function() {
+    $("#picture").empty()
     $(".doctor-info").show();
+    $("#name").text(doctorData.profile.first_name + " " + doctorData.profile.last_name);
     $("#gender").text(doctorData.profile.gender);
     $("#bio").text(doctorData.profile.bio);
+    $("#picture").append('<img src="' + doctorData.profile.image_url + '">')
   });
 };
 
 $(document).ready(function() {
   var myDoctor = new Doctor();
-  // var doctorArray = [];
-  // console.log("empty at top " + doctorArray);
-  $(".search").click(function() {
+  var doctorArray = [];
+  $("form#search-info").submit(function(event) {
+    event.preventDefault();
+    doctorArray = [];
     $(".results ul").empty();
+    ;
     var issue = $("#issue").val();
-    var doctorArray = myDoctor.getDoctors(issue);
-    console.log("populate array " + doctorArray);
+    doctorArray = myDoctor.getDoctors(issue);
+    $("#issue").val("");
+    $(".results").show();
+
     doctorArray.forEach(function(doctor) {
       displayDoctor(doctor);
     })
-    var doctorArray = [];
-    console.log("empty at end " + doctorArray);
-
-
-
-    // myDoctor.getDoctors(issue, displayDoctor);
   })
-
 })
--- js/doctor.js
+++ js/doctor.js
@@ -8,10 +8,11 @@ var doctorArray = [];
 Doctor.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(response) {
+    doctorArray = [];
     response.data.forEach(function(doctor) {
       doctorArray.push(doctor);
-      // console.log(JSON.stringify(response));
-    })
+      console.log(JSON.stringify(response));
+    });
   })
   .fail(function(error){
     $(".results").text("error");
