fcc70255fe1ed53591e7ff759bc55e1ac50d44bb
Nicole Sanders <nsanders9022@gmail.com>
2017-03-17 10:11:50 -0700

    finally get names to display on the page

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,13 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayDoctor = function(issue, doctorData) {
+  $(".results").append("The doctors that can help you with " + issue + " are: " + doctorData.profile.first_name);
+}
 
 $(document).ready(function() {
   var myDoctor = new Doctor();
   $(".search").click(function() {
     var issue = $("#issue").val();
-    myDoctor.getDoctors(issue);
+    myDoctor.getDoctors(issue, displayDoctor);
   })
 })
--- js/doctor.js
+++ js/doctor.js
@@ -3,14 +3,20 @@ var apiKey = require('./../.env').apiKey;
 var Doctor = function() {
 };
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+var doctorArray = [];
+
+Doctor.prototype.getDoctors = function(medicalIssue, displayFunction) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(response) {
+  .then(function(response) {
+    response.data.forEach(function(doctor) {
+      displayFunction(medicalIssue, doctor)
+
       console.log(JSON.stringify(response));
     })
-   .fail(function(error){
-      $("#issue").text(error.responseJSON.message);
+    .fail(function(error){
+      $(".results").text("error");
     });
+  });
 };
 
 
