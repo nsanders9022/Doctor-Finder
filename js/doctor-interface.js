c7147001398121750e39fef2ae249053e4a3c921
Nicole Sanders <nsanders9022@gmail.com>
2017-03-17 11:38:19 -0700

    add SASS to the h4 elements in the doctor info section

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,9 +1,11 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(issue, doctorData) {
+  $(".doctor-info").hide();
   $(".results ul").append('<li id=' + doctorData.profile.last_name +'> <a href="#">' + doctorData.profile.first_name + " " + doctorData.profile.last_name + '</a></li>');
   $("li#" + doctorData.profile.last_name).click(function() {
-    $("#gender").text("gender: " + doctorData.profile.gender);
+    $(".doctor-info").toggle();
+    $("#gender").text(doctorData.profile.gender);
     $("#bio").text(doctorData.profile.bio);
   });
 };
