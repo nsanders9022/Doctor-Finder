5df913d74e15f3af34e6838fdc9f56f5b0d1186a
Nicole Sanders <nsanders9022@gmail.com>
2017-03-17 10:38:06 -0700

    add li elements for each doctor result

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,13 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(issue, doctorData) {
-  $(".results").append("The doctors that can help you with " + issue + " are: " + doctorData.profile.first_name);
+  $(".results ul").append('<li>' + doctorData.profile.first_name + " " + doctorData.profile.last_name + '</li>');
 }
 
 $(document).ready(function() {
   var myDoctor = new Doctor();
   $(".search").click(function() {
+    $(".results ul").empty();
     var issue = $("#issue").val();
     myDoctor.getDoctors(issue, displayDoctor);
   })
