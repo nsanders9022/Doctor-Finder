8a89da54190e8ce264076016d2538234161c5759
Nicole Sanders <nsanders9022@gmail.com>
2017-03-17 11:05:07 -0700

    make getDoctors return a list of doctor objects. Front end doesn't work correctly yet

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,14 +1,23 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-var displayDoctor = function(issue, doctorData) {
-  $(".results ul").append('<li>' + doctorData.profile.first_name + " " + doctorData.profile.last_name + '</li>');
+var displayDoctor = function(array) {
+  array.forEach(function(doctor) {
+    $(".results ul").append('<li>' + doctor.profile.first_name + " " + doctor.profile.last_name + '</li>');
+  })
+  // $(".results ul").append('<li>' + doctorData.profile.first_name + " " + doctorData.profile.last_name + '</li>');
 }
 
 $(document).ready(function() {
   var myDoctor = new Doctor();
+  var doctorArray =[]
+  console.log("initiate: " + doctorArray);
   $(".search").click(function() {
     $(".results ul").empty();
     var issue = $("#issue").val();
-    myDoctor.getDoctors(issue, displayDoctor);
+    doctorArray = myDoctor.getDoctors(issue);
+    console.log("call getDoctors: " + doctorArray);
+    displayDoctor(doctorArray);
+    doctorArray = [];
+    console.log("empty at end: " + doctorArray);
   })
 })
