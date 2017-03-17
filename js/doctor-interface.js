286545c49c76d75d9ad77d4f02a2920032496477
Nicole Sanders <nsanders9022@gmail.com>
2017-03-17 11:20:59 -0700

    show doctor's gender and bio when the name is clicked on

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,8 +1,12 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(issue, doctorData) {
-  $(".results ul").append('<li>' + doctorData.profile.first_name + " " + doctorData.profile.last_name + '</li>');
-}
+  $(".results ul").append('<li id=' + doctorData.profile.last_name +'> <a href="#">' + doctorData.profile.first_name + " " + doctorData.profile.last_name + '</a></li>');
+  $("li#" + doctorData.profile.last_name).click(function() {
+    $("#gender").text("gender: " + doctorData.profile.gender);
+    $("#bio").text(doctorData.profile.bio);
+  });
+};
 
 $(document).ready(function() {
   var myDoctor = new Doctor();
@@ -11,4 +15,5 @@ $(document).ready(function() {
     var issue = $("#issue").val();
     myDoctor.getDoctors(issue, displayDoctor);
   })
+
 })
