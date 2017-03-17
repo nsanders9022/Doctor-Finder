a93f954b5fafb0dfc9c01a79f18cd64a32a128d3
Nicole Sanders <nsanders9022@gmail.com>
2017-03-17 08:42:21 -0700

    display a list of the doctor information in the console when an issue is searched

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,10 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+
+
+$(document).ready(function() {
+  var myDoctor = new Doctor();
+  $(".search").click(function() {
+    var issue = $("#issue").val();
+    myDoctor.getDoctors(issue);
+  })
+})
--- js/doctor.js
+++ js/doctor.js
@@ -1,12 +1,15 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getDoctors = function(medicalIssue) {
+var Doctor = function() {
+};
+
+Doctor.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+   .then(function(response) {
+      console.log(JSON.stringify(response));
     })
    .fail(function(error){
-      console.log("fail");
+      $("#issue").text(error.responseJSON.message);
     });
 };
 
